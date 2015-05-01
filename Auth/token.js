56cf7a578547f77273576259390d5b39badaf1dd
Jose Douglas Ramirez E <chibchombiano26@hotmail.com>
2015-04-30 20:38:51 -0500

    Standard

--- Auth/token.js
+++ Auth/token.js
@@ -2,12 +2,20 @@ angular.module('starter')
 .factory('tokenService', [function () {
  
     var tokenServiceFactory = {};
+    var tokenString;
  
     tokenServiceFactory.setTokenDocument = function(token){
-        if (token)
+        if (token){
             document.cookie = "BearerToken=" + token + "; path=/";
+            tokenString = token;
+        }
     }
- 
+
+    tokenServiceFactory.getTokenDocument = function(token){
+        if (tokenString)
+            return tokenString;
+    } 
  
     return tokenServiceFactory;
+    
 }]);
